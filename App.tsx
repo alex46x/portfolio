e9e8b8459ced9e54296de9f6a60bd84716efeb49
alex46x <alexdon9990@gmail.com>
2026-01-02 23:10:17 +0600

    3rd update

--- App.tsx
+++ App.tsx
@@ -217,9 +217,9 @@ const App: React.FC = () => {
           <div className="absolute inset-0 z-0 overflow-hidden">
             <img 
               
-              // src="IMG_20251216_125234.jpg" 
+              src="3.jpg" 
 
-              src="https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=2070&auto=format&fit=crop" 
+              {/* src="https://images.unsplash.com/photo-1550751827-4bd374c3f58b?q=80&w=2070&auto=format&fit=crop"  */}
               alt="Hero Cover" 
               className="w-full h-full object-cover grayscale opacity-20 transition-transform duration-1000 scale-105"
               style={{ transform: `translateY(${scrolled * 100}px)` }}
